82ec5dab061fbf6a94c2162fb36c91569fc0eca4
Brian Brooks <brooks.brian@gmail.com>
2014-09-19 00:47:59 -0400

    Add initial test framework.

--- examples/LinkerScript.hs
+++ examples/LinkerScript.hs
@@ -7,5 +7,5 @@ main :: IO ()
 main = do
   (f:_) <- getArgs
   contents <- readFile f
-  putStrLn $ LS.preprocess LS.defaultMacros f contents
-  -- putStrLn $ show $ LS.parseFile LS.defaultMacros f contents
+  -- putStrLn $ LS.preprocess [] f contents
+  putStrLn $ show $ LS.parseFile [] f contents
